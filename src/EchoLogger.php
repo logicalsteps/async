70d1936a94490411dbf3f2654dfdefeae63bf9e1
Arul Kumaran <arul@logicalsteps.net>
2018-09-25 19:41:56 +0800

    Simple logger to bundle along

---
+++ src/EchoLogger.php
@@ -0,0 +1,25 @@
+<?php
+
+
+namespace LogicalSteps\Async;
+
+
+use Psr\Log\AbstractLogger;
+
+class EchoLogger extends AbstractLogger
+{
+
+    /**
+     * Logs with an arbitrary level.
+     *
+     * @param mixed  $level
+     * @param string $message
+     * @param array  $context
+     *
+     * @return void
+     */
+    public function log($level, $message, array $context = array())
+    {
+        echo "[$level] $message";
+    }
+}
