bf0c71edcebce76cd050e088d0749eb00d65aa26
Arul Kumaran <arul@logicalsteps.net>
2018-09-25 22:27:26 +0800

    rearranged for ease of editing

--- test/test_execute.php
+++ test/test_execute.php
@@ -9,21 +9,6 @@ require __DIR__ . '/bootstrap.php';
 /** @var LoopInterface $loop */
 $loop = Factory::create();
 
-function flow()
-{
-    echo 'started' . PHP_EOL;
-    yield 'two_seconds';
-    echo 'after two seconds' . PHP_EOL;
-    yield 'four_seconds';
-    echo 'after four seconds' . PHP_EOL;
-    yield ['Timer', 'delay', 8];
-    echo 'after eight seconds' . PHP_EOL;
-    yield [new Timer(), 'delay', 3];
-    echo 'after three seconds' . PHP_EOL;
-
-    return 'completed';
-}
-
 function two_seconds(callable $call_back)
 {
     global $loop;
@@ -59,7 +44,20 @@ class Timer
     }
 }
 
+function flow()
+{
+    echo 'started' . PHP_EOL;
+    yield 'two_seconds';
+    echo 'after two seconds' . PHP_EOL;
+    yield 'four_seconds';
+    echo 'after four seconds' . PHP_EOL;
+    yield ['Timer', 'delay', 8];
+    echo 'after eight seconds' . PHP_EOL;
+    yield [new Timer(), 'delay', 3];
+    echo 'after three seconds' . PHP_EOL;
 
+    return 'completed';
+}
 $async = new Async();
 $async->setLoop($loop);
 $async->execute(flow(), 'var_dump');
