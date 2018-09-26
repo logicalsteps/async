02cd2c8d87906cc31f8387a9044811bcad82d4b5
Arul Kumaran <arul@logicalsteps.net>
2018-09-26 09:07:52 +0800

    Revert "improve exception handing by sending the unhandled exception to call back function and make call back logic consistent by making callback to return (error, result) instead of just result"
    This reverts commit 8619bd5554a8a1075e998116dd68bf87ae123d79.

--- src/Async.php
+++ src/Async.php
@@ -39,109 +39,103 @@ class Async implements LoggerAwareInterface
     private function run(Generator $flow, callable $callback = null)
     {
         try {
-            try {
-                if ($flow->valid()) {
-                    $value = $flow->current();
-                    $args  = [];
-                    $func  = [];
-                    if (is_array($value) && count($value) > 1) {
+            if ($flow->valid()) {
+                $value = $flow->current();
+                $args  = [];
+                $func  = [];
+                if (is_array($value) && count($value) > 1) {
+                    $func[] = array_shift($value);
+                    if (is_callable($func[0])) {
+                        $func = $func[0];
+                    } else {
                         $func[] = array_shift($value);
-                        if (is_callable($func[0])) {
-                            $func = $func[0];
-                        } else {
-                            $func[] = array_shift($value);
-                        }
-                        $args = $value;
-                    } else {
-                        $func = $value;
                     }
-                    if (is_callable($func)) {
-                        if ($this->logger) {
-                            if (is_array($func)) {
-                                $name = $func[0];
-                                if (is_object($name)) {
-                                    $name = '$' . lcfirst(get_class($name)) . '->' . $func[1];
-                                } else {
-                                    $name .= '::' . $func[1];
-                                }
-
-                            } else {
-                                $name = (string)$func;
-                            }
-                            $this->logger->info('await ' . $name . $this->format($args));
-                        }
-                        $args[] = function ($error, $result) use ($flow, $callback) {
-                            if ($error) {
-                                if ($this->logger) {
-                                    $this->logger->error((string)$error);
-                                }
-                                throw $error;
-                            }
-                            $flow->send($result);
-                            $this->execute($flow, $callback);
-                        };
-                        call_user_func_array($func, $args);
-                    } elseif ($value instanceof Generator) {
-                        if ($value->valid() && $this->logger) {
-                            $info = new ReflectionGenerator($value);
-                            $f    = $info->getFunction();
-                            if ($name = $info->getThis()) {
-                                if (is_object($name)) {
-                                    $name = '$' . lcfirst(get_class($name)) . '->' . $f->name;
-                                } else {
-                                    $name .= '::' . $f->name;
-                                }
-                            } else {
-                                $name = $f->name;
-                            }
-                            $args = [];
-                            foreach ($f->getParameters() as $parameter) {
-                                $args[] = '$' . $parameter->name;
-                            }
-                            $this->logger->info('await ' . $name . '(' . implode(', ', $args) . ');');
-                        }
-                        $this->execute($value, function ($value) use ($flow, $callback) {
-                            $flow->send($value);
-                            $this->execute($flow, $callback);
-                        });
-                    } else {
-                        $flow->send($value);
-                        $this->execute($flow);
-                    }
+                    $args = $value;
                 } else {
-                    $value = $flow->getReturn();
-                    if ($value instanceof Generator) {
-                        if ($value->valid() && $this->logger) {
-                            $info = new ReflectionGenerator($value);
-                            $f    = $info->getFunction();
-                            if ($name = $info->getThis()) {
-                                if (is_object($name)) {
-                                    $name = '$' . lcfirst(get_class($name)) . '->' . $f->name;
-                                } else {
-                                    $name .= '::' . $f->name;
-                                }
-                            } else {
-                                $name = $f->name;
-                            }
-                            $args = [];
-                            foreach ($f->getParameters() as $parameter) {
-                                $args[] = '$' . $parameter->name;
-                            }
-                            $this->logger->info('await ' . $name . '(' . implode(', ', $args) . ');');
-                        }
-                        $this->execute($value, $callback);
-                    } elseif (is_callable($callback)) {
-                        $callback(null, $value);
-                    }
+                    $func = $value;
                 }
-            } catch (Throwable $t) {
-                $flow->throw($t);
-                $this->execute($flow);
+                if (is_callable($func)) {
+                    if ($this->logger) {
+                        if (is_array($func)) {
+                            $name = $func[0];
+                            if (is_object($name)) {
+                                $name = '$' . lcfirst(get_class($name)) . '->' . $func[1];
+                            } else {
+                                $name .= '::' . $func[1];
+                            }
+
+                        } else {
+                            $name = (string)$func;
+                        }
+                        $this->logger->info('await ' . $name . $this->format($args));
+                    }
+                    $args[] = function ($error, $result) use ($flow, $callback) {
+                        if ($error) {
+                            if ($this->logger) {
+                                $this->logger->error((string)$error);
+                            }
+                            throw $error;
+                        }
+                        $flow->send($result);
+                        $this->execute($flow, $callback);
+                    };
+                    call_user_func_array($func, $args);
+                } elseif ($value instanceof Generator) {
+                    if ($value->valid() && $this->logger) {
+                        $info = new ReflectionGenerator($value);
+                        $f    = $info->getFunction();
+                        if ($name = $info->getThis()) {
+                            if (is_object($name)) {
+                                $name = '$' . lcfirst(get_class($name)) . '->' . $f->name;
+                            } else {
+                                $name .= '::' . $f->name;
+                            }
+                        } else {
+                            $name = $f->name;
+                        }
+                        $args = [];
+                        foreach ($f->getParameters() as $parameter) {
+                            $args[] = '$' . $parameter->name;
+                        }
+                        $this->logger->info('await ' . $name . '(' . implode(', ', $args) . ');');
+                    }
+                    $this->execute($value, function ($value) use ($flow, $callback) {
+                        $flow->send($value);
+                        $this->execute($flow, $callback);
+                    });
+                } else {
+                    $flow->send($value);
+                    $this->execute($flow);
+                }
+            } else {
+                $value = $flow->getReturn();
+                if ($value instanceof Generator) {
+                    if ($value->valid() && $this->logger) {
+                        $info = new ReflectionGenerator($value);
+                        $f    = $info->getFunction();
+                        if ($name = $info->getThis()) {
+                            if (is_object($name)) {
+                                $name = '$' . lcfirst(get_class($name)) . '->' . $f->name;
+                            } else {
+                                $name .= '::' . $f->name;
+                            }
+                        } else {
+                            $name = $f->name;
+                        }
+                        $args = [];
+                        foreach ($f->getParameters() as $parameter) {
+                            $args[] = '$' . $parameter->name;
+                        }
+                        $this->logger->info('await ' . $name . '(' . implode(', ', $args) . ');');
+                    }
+                    $this->execute($value, $callback);
+                } elseif (is_callable($callback)) {
+                    $callback($value);
+                }
             }
         } catch (Throwable $t) {
-            if (is_callable($callback)) {
-                $callback($t);
-            }
+            $flow->throw($t);
+            $this->execute($flow);
         }
     }
 
