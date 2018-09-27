a1d72970c9a4467da26c1e02f602402afc15fcd0
Arul Kumaran <arul@luracast.com>
2018-09-27 15:46:16 +0800

    improved echo logger

--- src/EchoLogger.php
+++ src/EchoLogger.php
@@ -10,6 +10,25 @@ class EchoLogger extends AbstractLogger
 {
     private $startTime;
 
+    private static $colors = [
+        'black' => 30,
+        'red' => 31,
+        'green' => 32,
+        'yellow' => 33,
+        'blue' => 34,
+        'magenta' => 35,
+        'cyan' => 36,
+        'lightGrey' => 37,
+        'grey' => 90,
+        'lightRed' => 91,
+        'lightGreen' => 92,
+        'lightYellow' => 93,
+        'lightBlue' => 94,
+        'lightMagenta' => 95,
+        'lightCyan' => 96,
+        'white' => 97
+    ];
+
     public function __construct()
     {
         $this->startTime = microtime(true);
@@ -20,9 +39,9 @@ class EchoLogger extends AbstractLogger
     /**
      * Logs with an arbitrary level.
      *
-     * @param mixed  $level
+     * @param mixed $level
      * @param string $message
-     * @param array  $context
+     * @param array $context
      *
      * @return void
      */
@@ -30,35 +49,42 @@ class EchoLogger extends AbstractLogger
     {
         switch ($message) {
             case 'start':
-                $message = ' ┌ ⚑ start';
+                $message = ' ┌▀' . $this->backgroundColor(' start ', 'green');
                 break;
             case 'end':
-                $message = ' └ ■ stop';
+                $message = ' └▄' . $this->backgroundColor(' stop ', 'lightRed');
                 break;
             default:
                 if ($this->consoleColors) {
-                    $message = str_replace('await ', $this->lightGrey('await '), $message);
+                    $message = str_replace('await ', $this->color('await ', 'lightGrey'), $message);
                 }
-                $depth   = $context['depth'] ?? 0;
-                $depth   = $depth ? ' │' . str_repeat(" ", $depth) . '├ ' : ' ├ ';
+                $depth = $context['depth'] ?? 0;
+                $depth = $depth
+                    ? ' │' . str_repeat(" ", $depth * 2) . $this->color('¤ ', 'yellow')
+                    : ' ├' . $this->color('» ', 'lightYellow');
                 $message = $depth . $message;
         }
         $elapsed = round((microtime(true) - $this->startTime));
         echo ' ' . str_pad((string)$elapsed, 5, '0', STR_PAD_LEFT)
-             . ' ' . $this->cyanBackground("[$level]") . $message . PHP_EOL;
+            . ' ' . $this->backgroundColor("[$level]", 'cyan') . $message . PHP_EOL;
     }
 
-    private function cyanBackground(string $text)
+    private function color(string $text, string $color)
     {
-        if ( ! $this->consoleColors) {
+        if (!$this->consoleColors) {
             return $text;
         }
-
-        return "\033[0;30m\033[46m{$text}\033[0m";
+        $code = static::$colors[$color];
+        return "\033[0;{$code}m{$text}\033[0m";
     }
 
-    private function lightGrey(string $text)
+    private function backgroundColor(string $text, string $color)
     {
-        return "\033[0;37m{$text}\033[0m";
+        if (!$this->consoleColors) {
+            return $text;
+        }
+        $code = static::$colors[$color] + 10;
+        return "\033[0;30m\033[{$code}m{$text}\033[0m";
     }
+
 }
