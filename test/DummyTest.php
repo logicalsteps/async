ef5fe5c1b94d0f96ae81bfb24954f7473337532c
aswinsatish <aswin@logicalsteps.net>
2018-10-23 11:30:55 +0530

    Update DummyTest.php
    Updated for newere versions

--- test/DummyTest.php
+++ test/DummyTest.php
@@ -4,7 +4,7 @@ namespace LogicalSteps\Async\Test;
 
 use LogicalSteps\Async\Async;
 
-class DummyTest extends \PHPUnit_Framework_TestCase
+class DummyTest extends \PHPUnit\Framework\TestCase
 {
     /**
      * A dummy test that calls a beacon method ensuring the class is autolaoded.
