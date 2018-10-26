dbf7784a2e3383a37fa46553d96aebcf709b9a90
Arul Kumaran <arul@logicalsteps.net>
2018-10-26 11:06:30 +0800

    test for async2

--- examples/example_001_delay_react.php
+++ examples/example_001_delay_react.php
@@ -1,6 +1,7 @@
 <?php
 
 use LogicalSteps\Async\Async;
+use LogicalSteps\Async\Async2;
 use LogicalSteps\Async\EchoLogger;
 use React\EventLoop\Factory;
 use React\EventLoop\LoopInterface;
@@ -58,24 +59,27 @@ class Timer
 
 function flow()
 {
-    //echo 'started' . PHP_EOL;
+    echo 'started' . PHP_EOL;
     yield 'two_seconds';
-    //echo 'after two seconds' . PHP_EOL;
+    echo 'after two seconds' . PHP_EOL;
     yield ['Timer', 'delay', 8];
-    //echo 'after eight seconds' . PHP_EOL;
+    echo 'after eight seconds' . PHP_EOL;
     $timer = new Timer();
     yield [$timer, 'wait', 3];
-    //echo 'after three seconds' . PHP_EOL;
+    echo 'after three seconds' . PHP_EOL;
     yield $timer->hold(1);
-    //echo 'after one second' . PHP_EOL;
+    echo 'after one second' . PHP_EOL;
     yield $timer->promise(2);
-    //echo 'after two seconds' . PHP_EOL;
+    echo 'after two seconds' . PHP_EOL;
     return $timer->hold(7);
 }
-
+/*
 $async = new Async(new EchoLogger());
 $async->setLoop($loop);
 $async->await(flow());
 //$async->execute(flow()); //run another session in parallel
+*/
 
+$async = new Async2();
+$async->_handleGenerator(flow())->then('var_dump');
 $loop->run();
