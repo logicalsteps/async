8dfdf3acc00bc9f8d4e44c335a7827fd48979395
Arul Kumaran <arul@logicalsteps.net>
2018-10-29 23:34:15 +0800

    two tests to begin with

---
+++ test/AsyncTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace LogicalSteps\Async\Test;
+
+use LogicalSteps\Async\Async;
+use React\Promise\PromiseInterface;
+use seregazhuk\React\PromiseTesting\TestCase;
+
+class AsyncTest extends TestCase
+{
+
+    public function testAwaitForSynchronousValue()
+    {
+        $promise = Async::await(7);
+        $this->assertInstanceOf(PromiseInterface::class, $promise);
+        $this->assertPromiseFulfillsWith($promise, 7);
+    }
+
+    public function testAwaitForCallback()
+    {
+        $later = function (callable $callback) {
+            sleep(1);
+            $callback(null, 14);
+        };
+
+        $promise = Async::await($later);
+        $this->assertInstanceOf(PromiseInterface::class, $promise);
+        $this->assertPromiseFulfillsWith($promise, 14);
+    }
+
+    /*
+    public function testSetLogger()
+    {
+
+    }
+    */
+}
