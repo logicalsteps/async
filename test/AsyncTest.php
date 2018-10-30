e390dffab331f22685caa551390ace841f70d3e0
Arul Kumaran <arul@logicalsteps.net>
2018-10-30 15:19:03 +0800

    added tests for supported promises

--- test/AsyncTest.php
+++ test/AsyncTest.php
@@ -2,7 +2,11 @@
 
 namespace LogicalSteps\Async\Test;
 
+use Amp\Success as AmpSuccess;
+use GuzzleHttp\Promise\FulfilledPromise as GuzzleFulfilledPromise;
+use Http\Promise\FulfilledPromise as HttplugFulfilledPromise;
 use LogicalSteps\Async\Async;
+use React\Promise\Promise as ReactPromise;
 use React\Promise\PromiseInterface;
 use seregazhuk\React\PromiseTesting\TestCase;
 
@@ -30,13 +34,56 @@ class AsyncTest extends TestCase
 
     public function testAwaitForGenerator()
     {
-        function gen () {
+        function gen()
+        {
             return yield 28;
-        };
+        }
 
         $promise = Async::await(gen());
         $this->assertInstanceOf(PromiseInterface::class, $promise);
         $this->assertPromiseFulfillsWith($promise, 28);
+    }
+
+    public function testAwaitForReactPromise()
+    {
+        $knownPromise = new ReactPromise(function ($resolver) {
+            $resolver('react_promise');
+        });
+
+        $promise = Async::await($knownPromise);
+        $this->assertInstanceOf(PromiseInterface::class, $promise);
+        $this->assertPromiseFulfillsWith($promise, 'react_promise');
+    }
+
+    public function testAwaitForAmpPromise()
+    {
+        $knownPromise = new AmpSuccess('amp_promise');
+
+        $promise = Async::await($knownPromise);
+        $this->assertInstanceOf(PromiseInterface::class, $promise);
+        $this->assertPromiseFulfillsWith($promise, 'amp_promise');
+    }
+
+    public function testAwaitForGuzzlePromise()
+    {
+        $knownPromise = new GuzzleFulfilledPromise('guzzle_promise');
+
+        $promise = Async::await($knownPromise);
+        $promise->then(function ($value) {
+            $this->assertEquals($value, 'guzzle_promise');
+        });
+        $this->assertInstanceOf(PromiseInterface::class, $promise);
+    }
+
+    public function testAwaitForHttplugPromise()
+    {
+        $knownPromise = new HttplugFulfilledPromise('httplug_promise');
+
+        $promise = Async::await($knownPromise);
+        $promise->then(function ($value) {
+            $this->assertEquals($value, 'httplug_promise');
+        });
+        $this->assertInstanceOf(PromiseInterface::class, $promise);
     }
 
     /*
